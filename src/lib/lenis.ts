43c90b714427235b4a5c820110423dab50c4516d
Shahriar <arfin17310@gmail.com>
2025-04-27 12:50:21 +0600

    fix(scrolling): lenis smooth scrolling

--- src/lib/lenis.ts
+++ src/lib/lenis.ts
@@ -1,3 +1,28 @@
 "use client";
+import React from "react";
+import Lenis, { LenisOptions } from "lenis";
 
-export { ReactLenis } from "lenis/react";
+interface ReactLenisProps {
+   root?: boolean;
+   children: React.ReactNode;
+   options?: LenisOptions;
+}
+
+export const ReactLenis = ({ children, options, root = true }: ReactLenisProps) => {
+   React.useEffect(() => {
+      const lenis = new Lenis({ autoRaf: true, ...options });
+
+      const raf = (time: number) => {
+         lenis.raf(time);
+         requestAnimationFrame(raf);
+      };
+
+      requestAnimationFrame(raf);
+
+      return () => {
+         lenis.destroy();
+      };
+   }, [options, root]);
+
+   return children;
+};
